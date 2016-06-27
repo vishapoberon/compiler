99bd3b3105fb6d8d2817ea87166ba9b6e7694632
David C W Brown <dave@brownsmeet.com>
2016-06-27 20:46:43 +0100

    Checkin real (not merged) postpush.pl

--- src/tools/testcoordinator/postpush.pl
+++ src/tools/testcoordinator/postpush.pl
@@ -18,17 +18,6 @@ sub writelog {
   close(LOG);
 }
 
-sub writelog {
-  my ($msg) = @_;
-
-  open(LOG, ">>/tmp/postpush.log") or die "Could not create postpush.log";
-  flock(LOG, 2)                    or die "Could not lock postpush.log";
-  print LOG sprintf("%s %s\n", strftime("%Y/%m/%d %H.%M.%S", localtime), $msg);
-  close(LOG);
-}
-
-writelog "Postpush.";
-
 my $postdata = from_json(param('POSTDATA'));
 
 my $url  = $postdata->{'repository'}->{'url'};
@@ -40,8 +29,8 @@ my $repo   = $url;  $repo   =~ s'^.*\/'';
 
 #my $repo="repo"; my $branch="branch"; my $name="name";
 
-<<<<<<< HEAD
 writelog "Post push github web hook for repository $repo, branch $branch, name $name.";
+
 
 my $child = fork;
 if (not defined $child) {die "Fork failed.";}
@@ -53,18 +42,8 @@ if ($child) {
   exec 'ssh root@oberon "perl vishap/voc/src/tools/testcoordinator/buildall.pl >/tmp/buildall.log &"';
   exit;
 }
-=======
-writelog "Repository $repo, branch $branch, name $name.";
->>>>>>> 3a17e1a65685db28158fd65fff295e93a1bec31f
 
 print header(),
   start_html("Vishap Oberon github post push web hook."),
   p("Repository $repo, branch $branch, name $name."),
   end_html();
-<<<<<<< HEAD
-=======
-
-system 'ssh root@oberon perl vishap/voc/src/tools/testcoordinator/buildall.pl >/tmp/buildall.log &';
-
-writelog "Buildall triggered."
->>>>>>> 3a17e1a65685db28158fd65fff295e93a1bec31f
