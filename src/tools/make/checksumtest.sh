ec7888859c405129d24d1afd95f765ba33340a68
David Brown <dave@brownsmeet.com>
2016-07-01 15:16:11 +0100

    Omit OPC and OPM from checksums as they contain the build date.

--- src/tools/make/checksumtest.sh
+++ src/tools/make/checksumtest.sh
@@ -1,14 +1,16 @@
 # Checksum tests
 
-md5=$((which md5;which md5sum)2>/dev/null)
+md5=md5
+if which md5sum >/dev/null 2>&1; then md5=md5sum; fi
 if [ -f $1 ]
 then
-  $md5 *.o >newsums
+  $md5 *.o | egrep -v "OP[CM]\\.o" >newsums
   if diff -b $1 newsums
-  then printf "\n--- Object file checksums match ---\n"; rm newsums
-else printf "\n--- Object file checksum mismatch ---\n"; rm newsums; exit 1
+  then printf "\n--- Object file checksums match ---\n"
+  else printf "\n--- Object file checksum mismatch ---\n"
   fi
+  rm newsums
 else
-  $md5 *.o >$1
+  $md5 *.o | egrep -v "OP[CM]\\.o" >$1
   printf "\n--- Object files checksummed ---\n"
 fi
