b5dbf13af4a7ab91dd7c0b44c571b08cd1286d67
David Brown <dave@brownsmeet.com>
2016-07-15 15:30:26 +0100

    Try using git pull to get updated buildall.pl as raw fetch is never up to date.

--- src/tools/make/postpush.pl
+++ src/tools/make/postpush.pl
@@ -43,9 +43,9 @@ if ($buildneeded) {
     writelog "Started buildall, pid = $child.";  # parent process
   } else {
     close(STDIN); close(STDOUT); close(STDERR);  # child process
-    # unlink "buildall.pl";
-    sleep 10;  # Leave time for github to have the new version ready. (Yuk!)
-    system 'curl https://raw.githubusercontent.com/vishaps/voc/master/src/tools/make/buildall.pl >buildall.pl';
+    #sleep 5;  # Leave time for github to have the new version ready. (Yuk!)
+    system 'cd voc && git pull';
+    system 'cd ~ && cp voc/src/tools/make/buildall.pl .';
     exec 'perl buildall.pl >/tmp/buildall.log';
     exit;
   }
