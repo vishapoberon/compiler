1ae3a2ff328ec92e3c4f25406c79216bc10671d6
David Brown <dave.github@brownsmeet.com>
2016-12-03 18:43:13 +0000

    Simplify ARGV setup. Add trial binary directory discovery code.

--- src/test/confidence/intsyntax/test.sh
+++ src/test/confidence/intsyntax/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 . ../testenv.sh
 # Generate mixed source and assembly code listing
-$OBECOMP IntSyntax.mod -fm >result
+$OBECOMP IntSyntax.mod -fm | fgrep -v "Testing. Binary directory is: '" >result
 . ../testresult.sh
