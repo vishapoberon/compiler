e6c858e6e78f0677d1862f99ad1d8d91b488b160
David Brown <dave.github@brownsmeet.com>
2016-12-04 17:56:06 +0000

    Prototype for finding installation based on binary name.

--- src/test/confidence/intsyntax/test.sh
+++ src/test/confidence/intsyntax/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 . ../testenv.sh
 # Generate mixed source and assembly code listing
-$OBECOMP IntSyntax.mod -fm | fgrep -v "Testing. Binary directory is: '" >result
+$OBECOMP IntSyntax.mod -fm >result
 . ../testresult.sh
