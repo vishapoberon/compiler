f3ff731794d9c539c222e8606b5336752c6e3cf9
David Brown <dave.github@brownsmeet.com>
2016-12-07 17:18:00 +0000

    Working on autobuild.

--- src/tools/autobuild/build-oberon.sh
+++ src/tools/autobuild/build-oberon.sh
@@ -5,11 +5,12 @@
 # $1 - whether to use sudo
 # $2 - build directory
 # $3 - CC
+# $4 - branch
 
 if test "$1" = "sudo"; then sudo=sudo; else sudo=""; fi
 
 echo ""
-echo === build-oberon.sh: \$1="$1", \$2="$2", \$3="$3", \$sudo="$sudo" ===
+echo === build-oberon.sh: \$1="$1", \$2="$2", \$3="$3", \$4="$4", \$sudo="$sudo" ===
 echo ""
 
 set -x
@@ -18,7 +19,7 @@ cd $2
 $sudo git reset --hard    # Clear the staging area
 $sudo git clean -dfx      # Remove all untracked files
 $sudo git pull            # Update the local repository
-$sudo git checkout -f $1  # Switch to requested branch
+$sudo git checkout -f $4  # Switch to requested branch
 
 export CC=$3
 $sudo make full
