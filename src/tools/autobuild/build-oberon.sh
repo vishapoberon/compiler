4dac1f29d50a416587957391f7f711fc39090c76
David Brown <dave.github@brownsmeet.com>
2016-12-22 14:42:53 +0000

    Fix conditional syntax in autobuild script.

--- src/tools/autobuild/build-oberon.sh
+++ src/tools/autobuild/build-oberon.sh
@@ -25,7 +25,8 @@ export CC=$3
 
 if test "$3" = "msc-x86"; then
   cmd /c 'C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat' x86 '&&' cd 'c:\cygwin\home\dave\msc-x86\voc' '&&' make full
-else; if test "$3" = "msc-x64"; then
+else
+if test "$3" = "msc-x64"; then
   cmd /c 'C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat' x64 '&&' cd 'c:\cygwin\home\dave\msc-x64\voc' '&&' make full
 else
   $sudo make full
